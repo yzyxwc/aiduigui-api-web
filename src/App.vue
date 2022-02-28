0c24f469a0e9dc14771b13456a6acce2487a88e6
wuchao <wuc@koal.com>
2022-02-28 10:31:02 +0800

    Infomation

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="fade">
+    <router-view v-if="noRefresh" ></router-view>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  provide() {
+    return {
+      reload: this.reload
+    }
+  },
+  data() {
+    return {
+      noRefresh: true
+    }
+  },
+  methods:{
+    reload() {
+      this.noRefresh = false
+      this.$nextTick(function(){
+        this.noRefresh = true
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.vue-treeselect__control{
+  height: 20px !important
+}
+
+body .el-table th.gutter{
+  display: table-cell!important;
+}
+
+body .el-table colgroup.gutter{
+  display: table-cell!important;
+}
+
+</style>
+
---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-button @click="cachData">缓存</el-button>
+    <el-button @click="getData" type="primary">获取</el-button>
+    <el-button :plain="true" @click="open">打开消息提示</el-button>
+    <p>这里是首页，可以放置一些公司的相关信息，简介等资料</p>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {}
+  },
+  methods:{
+    cachData(){
+      sessionStorage.setItem('currentUserName',"wc")
+    },
+    open() {
+      this.$message('这是一条消息提示');
+    },
+
+    getData(){
+      let currentUserName = sessionStorage.getItem('currentUserName')
+      this.$message.error(currentUserName)
+    }
+
+  }
+}
+</script>
+<style>
+#main {
+  color: red;
+}
+#main p {
+  background: #bbffaa;
+}
+</style>
